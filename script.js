916d1138e8721d7d7b66d9e1aacb72108f9e940d
Bostjan <bostjanzup@gmail.com>
2021-01-31 11:09:01 +0100

    Buttons for changing pen colour

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 let penDown = false;
+let colorOfPen = "black";
 let drawingZone = document.querySelector("#drawingZone");
 let clearButton = document.querySelector("#clearButton");
 let updateGridButton = document.querySelector("#updateGridButton");
+let blackColorPenButton = document.querySelector("#blackColorPen");
+let rainbowColorPenButton = document.querySelector("#rainbowColorPen");
+let addHuePenButton = document.querySelector("#addHuePen");
+
 clearButton.addEventListener("click", clearAllSquares);
 updateGridButton.addEventListener("click", updateGrid);
+blackColorPenButton.addEventListener("click", updatePenColor);
+rainbowColorPenButton.addEventListener("click", updatePenColor);
+addHuePenButton.addEventListener("click", updatePenColor);
+
+updateGrid(null);
 
 
-createGrid();
-
-// Create grid
-function updateGrid() {
+function updatePenColor(e) {
+    let penColor = e.target.id;
+    console.log(penColor);
+}
+function updateGrid(e) {
     // delete previous
     let drawingZone = document.querySelector("#drawingZone");
     let childrenToRemove = drawingZone.children;
@@ -19,8 +30,16 @@ function updateGrid() {
             childrenToRemove[i].remove();
         }
     }
-
-    let newNumberOfRows = prompt("How many rows or columns?", "10");
+    let newNumberOfRows;
+    if (e == null) {
+        newNumberOfRows = 10;
+    }
+    else {
+        do {
+            newNumberOfRows = prompt("How many rows or columns?");
+        }
+        while (newNumberOfRows <= 0 || newNumberOfRows > 100);
+    }
     let widthOfDrawingZone = getComputedStyle(drawingZone).width;
     widthOfDrawingZone = widthOfDrawingZone.substring(0, widthOfDrawingZone.length - 2);
     let singleSquaresNo = newNumberOfRows * newNumberOfRows;
@@ -39,20 +58,17 @@ function updateGrid() {
     allSquares.forEach(singleSquare => singleSquare.style.height = `${widthOfDrawingZone / newNumberOfRows}px`); */
 
 }
-
 function colorSingleSquare(e) {
     if (e.buttons == "1") {
         e.target.style.backgroundColor = "black";
     }
     e.preventDefault();
 }
-
 function colorHoveredSquare(e) {
     if (e.buttons == "1") {
         e.target.style.backgroundColor = "black";
     }
 }
-
 function clearAllSquares() {
     let allSquares = document.querySelectorAll("#singleSquare");
     allSquares.forEach(singleSquare => singleSquare.style.backgroundColor = "White");
